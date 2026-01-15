5d2edda8038f44412898d75738ccb47c371acd07
Fenitra12 <fenitra00@gmail.com>
2026-01-15 16:06:14 +0300

    a

--- src/main/java/com/aerienne/gestion/repository/compagnies/CompagnieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aerienne.gestion.repository.compagnies;
-
-import com.aerienne.gestion.model.compagnies.Compagnie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompagnieRepository extends JpaRepository<Compagnie, Long> {
-}
